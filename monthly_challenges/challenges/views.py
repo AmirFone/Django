6a617e26eefb4a807e940aa7b84587ef03881cbe
AmirFone <ahossain20@fordham.edu>
2023-01-27 13:35:30 -0500

    added Dynamic support for different months

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,24 @@
+from modulefinder import ReplacePackage
+from django.http import HttpResponse, HttpResponseNotFound, response
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def month_challenge(req,month):
+    # options=['January', 'February', 'March', 'April', 'May', 
+    #          'June', 'July', 'August', 'September', 
+    #          'October', 'November','December']
+    Response=None
+    if month=='January':
+        Response="Eat no meat for the entire month"
+    
+    elif month=='February':
+        Response="walk for at least 20 minutes every day!"
+    
+    elif month=='March':
+        Response='Learn django for at lest 20 minutes every day!'
+    
+    else:
+        return HttpResponseNotFound("this month is not supported!")
+    return HttpResponse(Response)

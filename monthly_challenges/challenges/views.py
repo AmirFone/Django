0bc1d0062b1ce6458ef64140a3f76478061b5d27
AmirFone <ahossain20@fordham.edu>
2023-01-27 16:30:21 -0500

    changeing the month_challenge function

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,22 +3,21 @@ from django.http import HttpResponse, HttpResponseNotFound, response
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+options={'January':"Eat no meat for the entire month", 
+         'February':"walk for at least 20 minutes every day!", 
+         'March':'Learn django for at lest 20 minutes every day!', 
+         'April':"Eat no meat for the entire month", 
+         'May':"walk for at least 20 minutes every day!", 
+         'June':'Learn django for at lest 20 minutes every day!', 
+         'July':"Eat no meat for the entire month",
+         'August':"walk for at least 20 minutes every day!",
+         'September':'Learn django for at lest 20 minutes every day!', 
+         'October':"Eat no meat for the entire month",
+         'November':"walk for at least 20 minutes every day!",
+         'December':'Learn django for at lest 20 minutes every day!'}
 
 def month_challenge(req,month):
-    # options=['January', 'February', 'March', 'April', 'May', 
-    #          'June', 'July', 'August', 'September', 
-    #          'October', 'November','December']
-    Response=None
-    if month=='January':
-        Response="Eat no meat for the entire month"
-    
-    elif month=='February':
-        Response="walk for at least 20 minutes every day!"
-    
-    elif month=='March':
-        Response='Learn django for at lest 20 minutes every day!'
-    
+    if month in options:
+        return HttpResponse(options[month])
     else:
         return HttpResponseNotFound("this month is not supported!")
-    return HttpResponse(Response)

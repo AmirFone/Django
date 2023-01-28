0dce398376accd6575fd2f0f7174b4ad41dea81b
AmirFone <ahossain20@fordham.edu>
2023-01-28 15:03:19 -0500

    adding new templates

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response
-from django.urls import reverse
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.template.loader import render_to_string
+from django.http import Http404,HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -19,18 +20,13 @@ monthly_challenges = {
 }
 
 # Create your views here.
-def index(req):
-    list_items=""
-    months=list(monthly_challenges.keys())
-    # for month in months:
-    #     cap_month=month.capitalize()
-    #     month_path=reverse("month-challenge",args=[month])
-    #     list_items+=f"<li><a href=\"{month_path}\">{cap_month}</a></li>"
-    # data=f"<ul>{list_items}</ul>"
-    return render(req,'challenges/index.html',{
-        "months":months
-    })
 
+def index(request):
+    months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -39,16 +35,16 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month")
 
     redirect_month = months[month - 1]
-    redirect_path= reverse('month-challenge',args=[redirect_month])
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
     return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request,'challenges/challenge.html',{
-            "text":challenge_text,
-            "month_this":month.capitalize()
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month
         })
-    except:
-        return HttpResponseNotFound("This month is not supported!")
+    except: 
+        raise Http404()
